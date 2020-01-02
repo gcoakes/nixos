7f98db5918058015341f8802282b81ecc0ef9fc7
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-01-01 18:21:41 -0800

    Changed to network manager for wireless control.

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -17,11 +17,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "laptop"; # Define your hostname.
-  networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here.  Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp3s0f2.useDHCP = true;
   networking.interfaces.wlp2s0.useDHCP = true;
