6e2377f41f6fccf4fef31003a97ac4bf762f73f9
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-03-15 20:06:21 -0500

    Reformatted with nixpkgs-fmt.

--- modules/common.nix
+++ modules/common.nix
@@ -1,88 +1,92 @@
 with builtins;
 let
-    gcoakes-nixhome =
-        if pathExists ("/home/gcoakes/.config/nixpkgs/home.nix") then (
-            "/home/gcoakes/.config/nixpkgs/home.nix"
-        ) else (
-            fetchGit {
-              url = https://gitlab.com/gcoakes/nixhome.git;
-              name = "gcoakes-nixhome";
-            } + "/home.nix"
-        );
-    home-manager =
-      if pathExists "/home/gcoakes/src/home-manager" then (
-        "/home/gcoakes/src/home-manager"
-      ) else (
-        fetchTarball {
-          url = https://github.com/rycee/home-manager/archive/master.tar.gz;
-        }
-      );
-in { config, lib, pkgs, ... }: {
-    imports = [
-        "${home-manager}/nixos"
-        ./entertainment.nix
-        ../cachix.nix
+  gcoakes-nixhome =
+    if pathExists ("/home/gcoakes/.config/nixpkgs/home.nix") then (
+      "/home/gcoakes/.config/nixpkgs/home.nix"
+    ) else (
+      fetchGit {
+        url = https://gitlab.com/gcoakes/nixhome.git;
+        name = "gcoakes-nixhome";
+      } + "/home.nix"
+    );
+  home-manager =
+    if pathExists "/home/gcoakes/src/home-manager" then (
+      "/home/gcoakes/src/home-manager"
+    ) else (
+      fetchTarball {
+        url = https://github.com/rycee/home-manager/archive/master.tar.gz;
+      }
+    );
+in
+{ config, lib, pkgs, ... }: {
+  imports = [
+    "${home-manager}/nixos"
+    ./entertainment.nix
+    ../cachix.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  programs.adb.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.gcoakes = {
+    shell = pkgs.zsh;
+    createHome = true;
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "docker" "networkmanager" "adbusers" ];
+  };
+
+  home-manager.users.gcoakes = import gcoakes-nixhome;
+
+  # Set your time zone.
+  time.timeZone = "US/Central";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    systemPackages = with pkgs; [
+      git
+      wget
+      htop
+      cachix
     ];
-
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-
-    programs.zsh = {
-      enable = true;
-      syntaxHighlighting.enable = true;
+    variables = {
+      PAGER = "less";
     };
+  };
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.gcoakes = {
-        shell = pkgs.zsh;
-        createHome = true;
-        isNormalUser = true;
-        extraGroups = [ "wheel" "audio" "docker" "networkmanager" ];
-    };
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system = {
+    autoUpgrade.enable = true;
+  };
 
-    home-manager.users.gcoakes = import gcoakes-nixhome;
+  # Allow yubikeys to be accessible.
+  services.udev.packages = with pkgs; [
+    android-udev-rules
+    yubikey-personalization
+  ];
 
-    # Set your time zone.
-    time.timeZone = "US/Central";
+  # Enable using smart cards.
+  services.pcscd.enable = true;
 
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment = {
-        systemPackages = with pkgs; [
-            git
-            wget
-            htop
-            cachix
-        ];
-        variables = {
-            PAGER = "less";
-        };
-    };
+  # Enable ssh connections from my smart card.
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    challengeResponseAuthentication = false;
+    permitRootLogin = "no";
+    ports = [ 2222 ];
+    authorizedKeysFiles = [ ".config/ssh/authorized_keys" ];
+  };
 
-    # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-    # servers. You should change this only after NixOS release notes say you
-    # should.
-    system = {
-        autoUpgrade.enable = true;
-    };
-
-    # Allow yubikeys to be accessible.
-    services.udev.packages = with pkgs; [
-        yubikey-personalization
-    ];
-
-    # Enable using smart cards.
-    services.pcscd.enable = true;
-
-    # Enable ssh connections from my smart card.
-    services.openssh = {
-      enable = true;
-      passwordAuthentication = false;
-      challengeResponseAuthentication = false;
-      permitRootLogin = "no";
-      ports = [ 2222 ];
-      authorizedKeysFiles = [ ".config/ssh/authorized_keys" ];
-    };
-
-    nix.gc.automatic = true;
+  nix.gc.automatic = true;
 }
