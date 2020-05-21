1eafb26ca78213a46957357db269cf571e0652a4
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-05-21 15:24:18 -0500

    Added jupyter service.

--- services.nix
+++ services.nix
@@ -1,18 +1,29 @@
 with builtins;
 { config, pkgs, ... }:
 let
-  myPython = pkgs.python3.withPackages
-    (ps: with ps;
-      [ ipykernel
-        ipykernel
-        ipywidgets
-        beautifulsoup4
-        lightning
-        matplotlib
-        seaborn
-        aiohttp
-      ]
-    );
+  rustKernel = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "evcxr";
+    version = "0.4.6";
+    src = pkgs.fetchFromGitHub {
+      owner = "google";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "12hlqgh74z8vmd7fkxh4vk3dqp8hlhzkxnbyywk6nphi562n6w5w";
+    };
+    cargoSha256 = "1vikhgy8ixhm28j5s19p9s1cxyyfj1dywyb20xajyhr5cij4blrm";
+    nativeBuildInputs = [ pkgs.cmake ];
+  };
+  myPython = pkgs.python3.withPackages (ps:
+    with ps; [
+      ipykernel
+      ipykernel
+      ipywidgets
+      beautifulsoup4
+      lightning
+      matplotlib
+      seaborn
+      aiohttp
+    ]);
 in {
   services.jupyter = {
     enable = true;
@@ -30,8 +41,19 @@ in {
           "{connection_file}"
         ];
         language = "python";
-        logo32 = "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-32x32.png";
-        logo64 = "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-64x64.png";
+        logo32 =
+          "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-32x32.png";
+        logo64 =
+          "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-64x64.png";
+      };
+      rust = {
+        displayName = "Rust";
+        argv = [
+          "${rustKernel}/bin/evcxr_jupyter"
+          "--control_file"
+          "{connection_file}"
+        ];
+        language = "rust";
       };
     };
   };
