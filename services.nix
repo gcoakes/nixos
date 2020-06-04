990c2b072a2e7cf48f9648f314880524edb8872d
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-06-04 15:08:59 -0500

    Used nixfmt.

--- services.nix
+++ services.nix
@@ -25,35 +25,37 @@ let
       aiohttp
     ]);
 in {
-  services.jupyter = {
-    enable = true;
-    notebookDir = "/mnt/work";
-    password = "'sha1:6d7e49cc865b:2f62311bd587a74d4b71595b7d67e0843e986448'";
-    port = 8888;
-    kernels = {
-      python3 = {
-        displayName = "Python 3";
-        argv = [
-          myPython.interpreter
-          "-m"
-          "ipykernel_launcher"
-          "-f"
-          "{connection_file}"
-        ];
-        language = "python";
-        logo32 =
-          "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-32x32.png";
-        logo64 =
-          "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-64x64.png";
-      };
-      rust = {
-        displayName = "Rust";
-        argv = [
-          "${rustKernel}/bin/evcxr_jupyter"
-          "--control_file"
-          "{connection_file}"
-        ];
-        language = "rust";
+  services = {
+    jupyter = {
+      enable = true;
+      notebookDir = "/mnt/work";
+      password = "'sha1:6d7e49cc865b:2f62311bd587a74d4b71595b7d67e0843e986448'";
+      port = 8888;
+      kernels = {
+        python3 = {
+          displayName = "Python 3";
+          argv = [
+            myPython.interpreter
+            "-m"
+            "ipykernel_launcher"
+            "-f"
+            "{connection_file}"
+          ];
+          language = "python";
+          logo32 =
+            "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-32x32.png";
+          logo64 =
+            "${myPython}/${myPython.sitePackages}/ipykernel/resources/logo-64x64.png";
+        };
+        rust = {
+          displayName = "Rust";
+          argv = [
+            "${rustKernel}/bin/evcxr_jupyter"
+            "--control_file"
+            "{connection_file}"
+          ];
+          language = "rust";
+        };
       };
     };
   };
