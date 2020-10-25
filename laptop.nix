9176f3362d7c8875fdd402f8eaf4bbcef6aebf6c
Gregory C. Oakes <gregoryoakes@fastmail.com>
2020-10-25 06:59:59 -0500

    Made dhcpcd not stall the boot process.

--- laptop.nix
+++ laptop.nix
@@ -4,6 +4,7 @@
     libinput.calibrationMatrix = "2.4 0 0 0 2.4 0 0 0 1";
   };
 
+  networking.dhcpcd.wait = "background";
   networking.interfaces.enp4s0f3u2.useDHCP = true;
   networking.interfaces.wlp2s0.useDHCP = true;
 
