0bf40118ce23abdbad06935088eae6ed1f072154
Gregory C. Oakes <gregoryx.oakes@intel.com>
2021-03-17 10:22:34 -0500

    Fix wsl building.

--- wsl-system.nix
+++ wsl-system.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, inputs, ... }: {
   boot.wsl = {
     enable = true;
     user = "oakesgrx";
@@ -10,7 +10,24 @@
   };
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.oakesgrx = import ./wsl-home.nix;
+  home-manager.users.oakesgrx = {
+    imports = [ (import ./dev-env.nix { email = "gregoryx.oakes@intel.com"; inherit inputs; }) ];
+
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    home.username = "oakesgrx";
+    home.homeDirectory = "/home/oakesgrx";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "21.05";
+  };
   networking.hostName = "nixos-wsl";
   networking.proxy = {
     default = "http://proxy-chain.intel.com:911";
