9bddfcbb40d6d7ccb8b0897fc76f4bf923840b9d
Gregory C. Oakes <gregcoakes@gmail.com>
2021-05-29 02:48:06 -0500

    Update vscode settings.

--- dev-env.nix
+++ dev-env.nix
@@ -17,6 +17,7 @@ in {
   home.packages = with pkgs; [
     cargo
     cargo-edit
+    clang-tools
     git-review
     kubectl
     kubernetes-helm
@@ -29,34 +30,41 @@ in {
     vscode = {
       enable = true;
       package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        a5huynh.vscode-ron
-        antfu.icons-carbon
-        bbenoist.Nix
-        brettm12345.nixfmt-vscode
-        dracula-theme.theme-dracula
-        eamodio.gitlens
-        elmtooling.elm-ls-vscode
-        esbenp.prettier-vscode
-        file-icons.file-icons
-        foxundermoon.shell-format
-        github.vscode-pull-request-github
-        jock.svg
-        justusadam.language-haskell
-        matklad.rust-analyzer
-        ms-azuretools.vscode-docker
-        ms-kubernetes-tools.vscode-kubernetes-tools
-        ms-python.vscode-pylance
-        ms-vscode.cpptools
-        redhat.vscode-yaml
-        serayuzgur.crates
-        tamasfe.even-better-toml
-        timonwong.shellcheck
-        tomoki1207.pdf
-        vadimcn.vscode-lldb
-        vscodevim.vim
-        yzhang.markdown-all-in-one
-      ];
+      extensions = with pkgs.vscode-extensions;
+        [
+          a5huynh.vscode-ron
+          antfu.icons-carbon
+          bbenoist.Nix
+          brettm12345.nixfmt-vscode
+          dracula-theme.theme-dracula
+          eamodio.gitlens
+          elmtooling.elm-ls-vscode
+          esbenp.prettier-vscode
+          file-icons.file-icons
+          foxundermoon.shell-format
+          github.vscode-pull-request-github
+          jock.svg
+          justusadam.language-haskell
+          matklad.rust-analyzer
+          ms-azuretools.vscode-docker
+          ms-kubernetes-tools.vscode-kubernetes-tools
+          ms-python.vscode-pylance
+          ms-vscode.cpptools
+          redhat.vscode-yaml
+          serayuzgur.crates
+          tamasfe.even-better-toml
+          timonwong.shellcheck
+          tomoki1207.pdf
+          vadimcn.vscode-lldb
+          vscodevim.vim
+          xaver.clang-format
+          yzhang.markdown-all-in-one
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          name = "nix-env-selector";
+          publisher = "arrterian";
+          version = "1.0.7";
+          sha256 = "0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
+        }];
     };
     zsh = {
       enable = true;
