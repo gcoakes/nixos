ff75e63044e13a5d952b71f9b96ccbc58960dc24
Gregory C. Oakes <gregcoakes@gmail.com>
2021-06-04 21:49:58 -0500

    Remove race conditions from xss-lock lockerCommand.

--- common.nix
+++ common.nix
@@ -196,7 +196,35 @@ in {
 
   programs.xss-lock = {
     enable = true;
-    lockerCommand = "${pkgs.i3lock-fancy}/bin/i3lock-fancy";
+    extraOptions = [ "--transfer-sleep-lock" ];
+    lockerCommand = let
+      locker = pkgs.writeShellScript "locker" ''
+        # We set a trap to kill the locker if we get killed, then start the locker and
+        # wait for it to exit. The waiting is not that straightforward when the locker
+        # forks, so we use this polling only if we have a sleep lock to deal with.
+        if [[ -e /dev/fd/${"$"}{XSS_SLEEP_LOCK_FD:--1} ]]; then
+            kill_i3lock() {
+                pkill -xu $EUID "$@" i3lock
+            }
+
+            trap kill_i3lock TERM INT
+
+            # we have to make sure the locker does not inherit a copy of the lock fd
+            ${pkgs.i3lock-fancy}/bin/i3lock-fancy {XSS_SLEEP_LOCK_FD}<&-
+
+            # now close our fd (only remaining copy) to indicate we're ready to sleep
+            exec {XSS_SLEEP_LOCK_FD}<&-
+
+            while kill_i3lock -0; do
+                sleep 0.5
+            done
+        else
+            trap 'kill %%' TERM INT
+            i3lock -n &
+            wait
+        fi
+      '';
+    in "${locker}";
   };
 
   services.xserver.xautolock = {
