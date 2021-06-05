a1ff352cd39d55d0bd95369d58c7c302df8b3ad4
Gregory C. Oakes <gregcoakes@gmail.com>
2021-06-04 20:53:58 -0500

    Encrypt laptop.

--- system/laptop.nix
+++ system/laptop.nix
@@ -12,13 +12,37 @@
   networking.hostName = "laptop";
   networking.dhcpcd.wait = "background";
 
-  swapDevices = [{ label = "swap"; }];
+  swapDevices = [{
+    encrypted = {
+      enable = true;
+      blkDev =
+        "/dev/disk/by-id/nvme-SKHynix_HFS256GD9TNG-L3A0B_AD99N802310409U50-part3";
+      # /mnt-root is the location where / will be mounted in stage 1.
+      keyFile = "/mnt-root/.swap-keyfile";
+      # This is the /dev/mapper/<label> not the filesystem level label.
+      label = "swap";
+    };
+    # Filesystem label.
+    label = "swap";
+  }];
 
   boot = {
     initrd = {
-      availableKernelModules =
-        [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-      kernelModules = [ ];
+      supportedFilesystems = [ "btrfs" ];
+      availableKernelModules = [ "nvme" "btrfs" ];
+      luks = {
+        gpgSupport = true;
+        devices.nixos = {
+          device =
+            "/dev/disk/by-id/nvme-SKHynix_HFS256GD9TNG-L3A0B_AD99N802310409U50-part2";
+          allowDiscards = true;
+          bypassWorkqueues = true;
+          gpgCard = {
+            encryptedPass = ../luks-passphrase.asc;
+            publicKey = ../public.asc;
+          };
+        };
+      };
     };
     kernelModules = [ "kvm-amd" ];
     kernelParams = [ "amd_iommu=on" ];
